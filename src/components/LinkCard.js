194e06d333da39202b336a355c4d2067f8077707
adecode <agorohabeeb@gmail.com>
2022-11-01 23:44:23 +0100

    added anchors links and ids

--- src/components/LinkCard.js
+++ src/components/LinkCard.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
-function LinkCard({title}) {
+function LinkCard({title, id, link}) {
   return (
-    <div className='flex justify-center items-center bg-gray h-[76px] w-[100%] mb-6 rounded-lg'>
-        <h3 className='font-medium text-lg text-text_color'>{title}</h3>
-    </div>
+    <Link to={link} className='font-medium text-lg text-text_color flex justify-center items-center bg-gray h-[76px] w-[100%] mb-6 rounded-lg'
+        id={id}
+    >
+        {title}
+    </Link>
+
   )
 }
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,13 +7,18 @@ function Profile() {
     <div className='flex-col w-[100%] align-middle'>
         <div className='flex-col justify-center items-center w-[100%] mb-14'>
             <div className='flex justify-center profle-image mb-6'>
-                <img className='w-[88px] h-[88px] rounded-full' src={pass} alt='profile'/>
+                <img className='w-[88px] h-[88px] rounded-full' src={pass} alt='profile' id='profile__img'/>
             </div>
-            <h2 className='flex justify-center font-bold text-lg'>Agoro Habeeb Adekorede</h2>
+            <h2 className='flex justify-center font-bold text-lg' id="twitter">adecode</h2>
+            <h2 className='flex justify-center font-bold text-lg hidden' id="slack">adecode</h2>
         </div>
         <div className='flex-col box-border px-36'>
-            <LinkCard title='Twitter Link'/>
+            <LinkCard title='Twitter Link' id='btn__zuri' link='https://training.zuri.team/'/>
             <LinkCard title='Zuri Team'/>
+            <LinkCard title='Zuri Books'/>
+            <LinkCard title='Python Books'/>
+            <LinkCard title='Background Check for Coders'/>
+            <LinkCard title='Design Books'/>
         </div>
     </div>
   )
